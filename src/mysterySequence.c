021e0fb7219a7a71a9c58fa8ef262c329322acf2
invader237 <diego.trivino.fr@gmail.com>
2023-11-12 21:06:46 +0100

    :construction:(mysterySequence.c):modification of the sequenceGenerator function to make it functional

--- src/mysterySequence.c
+++ src/mysterySequence.c
@@ -1,38 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-//#include <./../include/SDL2/SDL.h>
-
-
+#include <SDL.h>
 
 int randGenerator();
 int sequenceGenerator();
 int computeNextTerm();
 
+int* sequenceGenerator() {
 
+  int a = randGenerator(), b = randGenerator(), c = randGenerator();
+  int sequence[4] = {c};
+  
+  for (int i = 1; i <= 3; i++) {
+        //calculation of the following terms of the sequence based on the randomly generated terms
+        sequence[i] = computeNextTerm(sequence[i - 1], a, b);
+    }
+	printf("%d %d %d %d", sequence[0],sequence[1],sequence[2],sequence[3]);
+  return sequence;
 
-int sequenceGenerator() {
-  int* sequence[3] = {randGenerator(), randGenerator(), randGenerator()};
-  sequence[3] = computeNextTerm(sequence[0], sequence[1], sequence[2]);
-	printf("%d%d%d%d", sequence[0],sequence[1]sequence[2]sequence[3]);
-
-  return 0;
 }
 
 int computeNextTerm(int prevTerm, int a, int b) {
+
   //calculating the next term of the sequence
   return a * prevTerm + b;
+
 }
 
 int randGenerator() {
+
   //generates a random number between 1 and 10 with time as seed
 	int number = rand() % 10 + 1;
 	return number;
+
 }
 
 int main() {
   srand(time(NULL));
 	sequenceGenerator();
+
   /*if (SDL_Init(SDL_INIT_VIDEO) < 0) {
     fprintf(stderr, "Erreur lors de l'initialisation de SDL : %s\n", SDL_GetError());
     return -1;
