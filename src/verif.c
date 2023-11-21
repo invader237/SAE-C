82f88165c13c8e80e7d3a2dd55ca73417181ef08
guillaume pages <guigui97457@gmail.com>
2023-11-21 19:47:50 +0100

    :heavy_plus_sign:(verif.c):Added function for games

--- src/verif.c
+++ src/verif.c
@@ -1,17 +1,98 @@
 #include <stdio.h>
-#include <ctype.h>
 #include <stdlib.h>
+#include <ctype.h>
+#include <math.h>
+#include <time.h>
 
-int main() {
-  char chaine[100]; // tableau pouvant contenir 100 caractères //
-  int nombre;
+#define NB_VOYELLES (sizeof(vowel) / sizeof(char) - 1)
 
-  printf("Entrez une chaîne de caractères : ");
-  fgets(chaine, 100, stdin); // lis la chaine de caractère dans chaine //
+int verifNumber(double d)
+{
+    if (d == floor(d))
+        puts("Entier");
+    else
+        puts("Flottant");
 
-  nombre = strtol(chaine, NULL, 10); // permet de convertir une chaîne de caractères en un nombre entier //
-
-  printf("Le nombre est %d.\n", nombre);
-
-  return 0;
+    return 0;
 }
+
+char generateOneRandomVowel() {
+    char vowel[] ="AEIOUY";
+    int index = rand() % NB_VOYELLES;
+    return vowel[index];
+}
+
+char* generateVowelList() {
+    srand(time(NULL));
+    char* vowelListe = (char*)malloc(4 * sizeof(char)); // Ajout de l'espace pour le caractère nul
+    if (vowelListe == NULL) {
+        return NULL;
+    }
+
+    for (int i = 0; i <= 4; i++) {
+        vowelListe[i] = generateOneRandomVowel();
+    }
+
+    // Ajout du caractère nul à la fin de la liste
+    vowelListe[4] = '\0';
+
+    return vowelListe;
+}
+
+int verifVowel(char caracter) {
+    char vowel[] = "AEIOUY";
+    for (int i = 0; i < NB_VOYELLES; i++) {
+        if (caracter == vowel[i]) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int checkPosCorrect(char *user,char *vowelListe) {
+    // Compteur de voyelles correctement placées
+    int correctPos = 0;
+
+    // Comparaison des voyelles correctement placées
+    for (int i = 0; i < 4; i++) {
+        if (vowelListe[i] == user[i]) {
+            correctPos++;
+        }
+    }
+    return correctPos;
+}
+
+int checkPosIncorrect(char *user,char *vowelListe) {
+    // Compteur de voyelles présentes mais mal placées
+    int incorrectPos = 4;
+
+    // Comparaison des voyelles présentes mais mal placées
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            if (vowelListe[i] == user[j] && i != j) {
+                incorrectPos-=1;
+                break; // Sortir de la boucle interne dès qu'une correspondance est trouvée
+            }
+        }
+    }
+    return incorrectPos;
+}
+
+
+/*int main()
+{
+    // Vérification de la voyelle 'E'
+    verifVoyelles('E');
+
+    // Vérification du nombre 67
+    verifNumber('a');
+
+    // Génération de 4 voyelles
+    char* liste=generateVowelList();
+
+
+    // Vérification de la combinaison "AEOU"
+    printf("\n%d",checkPosCorrect("AEOU", liste));
+    printf("\n%d",checkPosIncorrect("AEOU", liste));
+
+}*/
